2c04541e53a62a064b0981fb544b15a205e88da7
User <nicitakormakov@gmail.com>
2020-06-09 16:58:22 +0500

    V1

---
+++ Project10.1/Project10.1/Main.c
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <stdlib.h>
+#include <windows.h>
+#include <locale.h>
+
+
+int main()
+{
+	setlocale(LC_ALL, "RUS");
+	HANDLE handle;
+	DWORD result;
+
+	handle = CreateMutex(NULL, TRUE, "MutexOfTheMyApplication");
+	result = WaitForSingleObject(handle, NULL);
+
+	if (result != WAIT_OBJECT_0)
+	{
+		printf("Программа уже запущена!\n");
+		system("pause");
+		return 0;
+	}
+	else
+	{
+		printf("Программа работает...\n");
+	}
+	_getche();
+}
---
+++ Project10/Project10/Main.c
@@ -0,0 +1,51 @@
+﻿#include <windows.h> 
+#include <stdio.h> 
+#include <psapi.h> 
+
+int Count = 0;
+
+void PrintProcessNameAndID(DWORD processID) 
+{
+	char szProcessName[257];
+	HANDLE hProcess = OpenProcess(PROCESS_QUERY_INFORMATION | PROCESS_VM_READ, FALSE, processID);
+
+	if (NULL != hProcess) 
+	{
+		HMODULE hMod;
+		DWORD cbNeeded;
+
+		if (EnumProcessModules(hProcess, &hMod, sizeof(hMod), &cbNeeded)) 
+		{
+			GetModuleBaseName(hProcess, hMod, szProcessName, sizeof(szProcessName) / sizeof(TCHAR));
+			printf("%s\n", szProcessName);
+			Count = Count + 1;
+		}
+	}
+
+	CloseHandle(hProcess);
+}
+
+int main() 
+{
+	DWORD aProcesses[1024], cbNeeded, cProcesses;
+	unsigned int i;
+
+	if (!EnumProcesses(aProcesses, sizeof(aProcesses), &cbNeeded))
+	{
+		return 1;
+	}
+
+	cProcesses = cbNeeded / sizeof(DWORD);
+	
+	for (i = 0; i < cProcesses; i++)
+	{
+		if (aProcesses[i] != 0)
+		{
+			PrintProcessNameAndID(aProcesses[i]);
+		}
+	}
+	printf("\n");
+	printf("Get access to %d from %d processes\n", Count, cProcesses);
+	getchar();
+	return 0;
+}
